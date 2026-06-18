ab4749bde54767bca678f6af02edeab5fa6271cb
Andisiwe <mafikaandisiwe@gmail.com>
2026-06-18 16:58:07 +0200

    Add files via upload
    the breakdown of products purchased together

---
+++ purchased together.sql
@@ -0,0 +1,12 @@
+/*purchase together*/;
+
+with prod_sales as (
+select ordernumber, t1.productcode, productLine
+from orderdetails t1
+inner join products t2
+on t1.productcode = t2.productcode
+)
+select distinct t1.ordernumber, t1.productline as product_one, t2.productline as product_two
+from prod_sales t1
+left join prod_sales t2
+on t1.ordernumber = t2.ordernumber and t1.productline <> t2.productline;
